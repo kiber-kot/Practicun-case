7a4cd98e14c073d8e4e3ae6d008e58e9cfa85f21
velzevul <ru@ru.ru>
2024-04-13 23:45:02 +0300

    Создал проект с обучением модели

---
+++ data_preprocessing.py
@@ -0,0 +1,52 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+
+# Предобработка данных для применения методов машинного обучения
+
+
+stress_level_data = pd.read_csv('stress_level.csv', sep=';')
+
+# Работа с пропущенными значениями (NaN)
+# Заполенение пропусков в столбце mental_health_history
+# Если надо удалить NAN в конкретно столбце то ко будет следующем
+delete_mental_health_history_NaN = stress_level_data.dropna(subset=['mental_health_history'])
+
+# Удаляет все пропущеные колонки NaN
+deleteAllNaN = stress_level_data.dropna()
+
+# Способ 2. Заполнение средним значением с импользованием fillna()
+stress_level_data['mental_health_history'] = stress_level_data.fillna(stress_level_data['stress_level'].mean)
+stress_level_data.head()
+
+# Способ 3. Заполнение часто встречающее значение
+
+imputer = SimpleImputer(strategy='most_frequent')
+stress_level_data[['mental_health_history']] = imputer.fit_transform(stress_level_data[['stress_level_data']])
+
+# Способ 4. Замена константой
+imputer = SimpleImputer(strategy='constant', fill_value=1)
+
+# Обнаружение и устранение выбрасов
+stress_level_data.describe()
+
+# Построение ящика с усами
+stress_level_data[['Название колонки']].boxplot()
+
+# Усечение значений
+# Сделаем усечение кол-во записей в конкретно колонки допустим до 10
+stress_level_data.loc[stress_level_data['название колонки'] > 10, 'название колонки'] = 10
+
+# Фильтрация колонки свыше 20 и вывести максимальное значение
+stress_level_data[stress_level_data['Название колонки'] < 20]['название колонки'].max()
+
+# Сделаем усечение кол-во записей в конкретно колонки допустим до 5
+stress_level_data.loc[stress_level_data['название колонки'] > 5, 'название колонки'] = 5
+
+# Масштабирование
+
+scaler = MinMaxScaler(feature_range=(-1, 1))
+
+stress_level_data[['Название колонки']] = scaler.fit_transform(stress_level_data[['Название колонки']])
---
+++ preprocessing.py
@@ -0,0 +1,61 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import MinMaxScaler, StandardScaler, RobustScaler
+
+array_full_value_data = ['anxiety_level', 'self_esteem', 'mental_health_history', 'depression', 'headache',
+                         'blood_pressure', 'sleep_quality', 'breathing_problem',
+                         'noise_level', 'living_conditions', 'safety', 'basic_needs', 'academic_performance',
+                         'study_load', 'teacher_student_relationship',
+                         'future_career_concerns', 'social_support', 'peer_pressure', 'extracurricular_activities',
+                         'bullying', 'stress_level']
+array_not_stress_level_value_data = ['anxiety_level', 'self_esteem', 'mental_health_history', 'depression', 'headache',
+                                     'blood_pressure', 'sleep_quality', 'breathing_problem',
+                                     'noise_level', 'living_conditions', 'safety', 'basic_needs',
+                                     'academic_performance', 'study_load', 'teacher_student_relationship',
+                                     'future_career_concerns', 'social_support', 'peer_pressure',
+                                     'extracurricular_activities', 'bullying']
+
+
+def delete_nan_value_csv(path):
+    """Убираем все записи, где значение пустое"""
+    stress_level_data = pd.read_csv(path, sep=';')
+    delete_NaN_data = stress_level_data.dropna()
+    return delete_NaN_data
+
+
+stress_level_data = delete_nan_value_csv('stress_level.csv')
+
+
+def truncation_values(stress_level_data):
+    """Усечение значений.
+    В файле имеются анамальные значение в колнках noise_level = 1111111 и future_career_concerns = 101010101"""
+    # Фильтрация колонки свыше 20 и вывести максимальное значение
+    value_noise_level = stress_level_data[stress_level_data['noise_level'] > 20]['noise_level'].max()
+    print('Значение в колонке value_noise_level котрое имеет аномалии = ', value_noise_level)
+    value_future_career_concerns = stress_level_data[stress_level_data['future_career_concerns'] > 20][
+        'future_career_concerns'].max()
+    print('Значение в колонке value_future_career_concerns котрое имеет аномалии = ', value_future_career_concerns)
+
+    # Определяем максимальные значение до усечения в колонке noise_level, где имеются анамалии
+    result_noise_level = stress_level_data[stress_level_data['noise_level'] < 20]['noise_level'].max()
+
+    # Сделаем усечение кол-во записей в колонки noise_level до result_noise_level = 5
+    stress_level_data.loc[stress_level_data['noise_level'] > result_noise_level, 'noise_level'] = result_noise_level
+
+    # Определяем максимальные значение до усечения в колонке future_career_concerns, где имеются анамалии
+    result_future_career_concerns = stress_level_data[stress_level_data['future_career_concerns'] < 20][
+        'future_career_concerns'].max()
+
+    # Сделаем усечение кол-во записей в колонки future_career_concerns до result_noise_level = 5
+    stress_level_data.loc[stress_level_data[
+                              'future_career_concerns'] > result_future_career_concerns, 'future_career_concerns'] = result_future_career_concerns
+    return stress_level_data
+
+
+def scaler(stress_level_data):
+    scaler = MinMaxScaler(feature_range=(0, 1))
+    stress_level_data[array_not_stress_level_value_data] = scaler.fit_transform(stress_level_data[array_not_stress_level_value_data])
+    return stress_level_data
+
